3248d537f3ed765854269e73ac52b13983984809
Anton Fagerberg <anton@antonfagerberg.com>
2013-10-31 19:26:21 +0100

    Minor fix.

--- quicksort.hs
+++ quicksort.hs
@@ -1,6 +1,5 @@
 quicksort :: Ord x => [x] -> [x]
-quicksort [] = []
-quicksort [x] = [x]
 quicksort (x:xs) = quicksort lte ++ [x] ++ quicksort gt
-    where lte = filter (<=x) xs
-          gt = filter (>x) xs
+    where lte = filter (<= x) xs
+          gt = filter (> x) xs
+quicksort x = x
